7439094d0c0fe35854308f317de30bd606820bfd
straleboni <straleboni@gmail.com>
2020-10-22 16:31:09 +0200

    Header

--- src/pages/shoppage/shoppage.component.js
+++ src/pages/shoppage/shoppage.component.js
@@ -2,20 +2,16 @@ import React from 'react';
 import SHOP_DATA from '../../shopData';
 import './shoppage.styles.scss';
 import CollectionPreview from '../../components/collection-preview/collection-preview';
-
 class ShopPage extends React.Component {
 
     state = {
         collections: SHOP_DATA
     }
-    handleState = () => {
-        console.log(this.state.collections)
-    }
+
     render(){
         const {collections} = this.state;
         return(
             <div>
-                {/* <CollectionPreview collections={this.state.collections}/> */}
                 {
                     collections.map(({id, ...otherCollectionProps}) => (
                         <CollectionPreview key={id} {...otherCollectionProps}/>
