785d0ada59e597239ff8fc807e75fc14fe15330c
straleboni <straleboni@gmail.com>
2020-10-22 18:05:38 +0200

    Sign Up form before Firebase setting up

---
+++ src/components/sign-in/sign-in.component.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+class SignIn extends React.Component {
+    state = {
+        email:'',
+        password: ''
+    }
+    handleOnChange = event => {
+        const {value,name} = event.target;
+        this.setState({ [name]:value });
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password:''});
+    }
+    render(){
+        const {email,password} = this.state;
+        return(
+            <div className="sign-in">
+                <h2>I already have an account?</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput label="email" type="email" name="email" value={email} onChange={this.handleOnChange} required/>
+                    <FormInput label="password" type="password" name="password" value={password} onChange={this.handleOnChange} required/>
+                    <CustomButton type="submit" value="Submit Form">SIGN IN</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+export default SignIn;
